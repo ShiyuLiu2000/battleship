2e9655dd33fcd12ec0eeb9c34e73444dd295a7e9
sl846 <shiyu.liu2@duke.edu>
2025-02-06 00:39:09 -0500

    make BattleShipBoard implement Board; modified tests

--- app/src/main/java/edu/duke/sl846/battleship/BattleShipBoard.java
+++ app/src/main/java/edu/duke/sl846/battleship/BattleShipBoard.java
@@ -3,14 +3,14 @@ package edu.duke.sl846.battleship;
 /**
  * Represents a Board in our battleship game.
  */
-public class BattleShipBoard {
+public class BattleShipBoard implements Board {
   private final int width;
   private final int height;
 
   /**
    * Constructs a Board with given width and height.
    * 
-   * @param width is the width of this Board.
+   * @param width  is the width of this Board.
    * @param height is the height of this Board.
    * @throws IllegalArgumentException if width or height is not positive.
    */
--- app/src/test/java/edu/duke/sl846/battleship/BattleShipBoardTest.java
+++ app/src/test/java/edu/duke/sl846/battleship/BattleShipBoardTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class BattleShipBoardTest {
   @Test
   public void test_get_valid_width_and_height() {
-    BattleShipBoard testBoard = new BattleShipBoard(10, 20);
+    Board testBoard = new BattleShipBoard(10, 20);
     assertEquals(10, testBoard.getWidth());
     assertEquals(20, testBoard.getHeight());
   }

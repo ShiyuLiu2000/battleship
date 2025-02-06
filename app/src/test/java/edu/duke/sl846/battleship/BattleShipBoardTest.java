d4b40a3d28d8d5b8484060db91e7fd079b19b74e
sl846 <shiyu.liu2@duke.edu>
2025-02-05 22:22:18 -0500

    wrote tests for BattleshipBoard getters with illegal argument

--- app/src/test/java/edu/duke/sl846/battleship/BattleShipBoardTest.java
+++ app/src/test/java/edu/duke/sl846/battleship/BattleShipBoardTest.java
@@ -6,9 +6,19 @@ import org.junit.jupiter.api.Test;
 
 public class BattleShipBoardTest {
   @Test
-  public void test_get_width_and_height() {
+  public void test_get_valid_width_and_height() {
     BattleShipBoard testBoard = new BattleShipBoard(10, 20);
     assertEquals(10, testBoard.getWidth());
     assertEquals(20, testBoard.getHeight());
+  }
+
+  @Test
+  public void test_invalid_dimensions() {
+    // JUnit will execute the lambda (second parameter) inside a try-catch, and make
+    // sure it catches the exception class we specify
+    assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard(10, 0));
+    assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard(0, 20));
+    assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard(-3, 5));
+    assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard(5, -9));
   }
 }

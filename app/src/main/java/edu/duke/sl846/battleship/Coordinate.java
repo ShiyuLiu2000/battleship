8c573d607a331c9a78e1b93994db95999f828526
sl846 <shiyu.liu2@duke.edu>
2025-02-06 22:18:13 -0500

    added toString and hashCode to Coordinate, passed tests

--- app/src/main/java/edu/duke/sl846/battleship/Coordinate.java
+++ app/src/main/java/edu/duke/sl846/battleship/Coordinate.java
@@ -38,6 +38,10 @@ public class Coordinate {
 
   /**
    * Compares two Coordinates to see if they have equality.
+   * 
+   * @param object is the Object being compared to.
+   * @return true if the two Coordinates have the same row number and column
+   *         number, false otherwise.
    */
   @Override
   public boolean equals(Object object) {
@@ -46,5 +50,26 @@ public class Coordinate {
       return row == coordinate.row && column == coordinate.column;
     }
     return false;
+  }
+
+  /**
+   * Gives textual representation of the Coordinate, e.g. (1,2)
+   * 
+   * @return textual representation of the Coordinate.
+   */
+  @Override
+  public String toString() {
+    return "(" + row + ", " + column + ")";
+  }
+
+  /**
+   * Generates hashcode for a Coordinate.
+   * (Use the fact that Java's String have a good hashcode)
+   * 
+   * @return hashcode of a Coordinate.
+   */
+  @Override
+  public int hashCode() {
+    return toString().hashCode();
   }
 }
--- app/src/test/java/edu/duke/sl846/battleship/CoordinateTest.java
+++ app/src/test/java/edu/duke/sl846/battleship/CoordinateTest.java
@@ -5,6 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class CoordinateTest {
+  Coordinate c1 = new Coordinate(0, 0);
+  Coordinate c2 = new Coordinate(0, 0);
+  Coordinate c3 = new Coordinate(1, 5);
+  Coordinate c4 = new Coordinate(1, 5);
+  Coordinate c5 = new Coordinate(5, 1);
+
   @Test
   public void test_get_row_and_column() {
     Coordinate testCoordinate = new Coordinate(3, 4);
@@ -14,16 +20,26 @@ public class CoordinateTest {
 
   @Test
   public void test_equals() {
-    Coordinate c1 = new Coordinate(0, 0);
-    Coordinate c2 = new Coordinate(0, 0);
-    Coordinate c3 = new Coordinate(1, 5);
-    Coordinate c4 = new Coordinate(1, 5);
-    Coordinate c5 = new Coordinate(5, 1);
+    assertTrue(c1.equals(c1)); // should be reflexive
     assertTrue(c1.equals(c2));
     assertTrue(c3.equals(c4));
     assertFalse(c2.equals(c5));
     assertFalse(c5.equals(c4));
-    Board b = new BattleShipBoard(3, 4);
-    assertFalse(c4.equals(b));
+    assertNotEquals(c4, "(1, 5)");
+    assertEquals(c2, c1);
+  }
+
+  @Test
+  public void test_toString() {
+    assertEquals("(0, 0)", c2.toString());
+    assertEquals(c3.toString(), "(1, 5)");
+  }
+
+  @Test
+  public void test_hashCode() {
+    assertEquals(c1.hashCode(), c2.hashCode());
+    assertEquals(c3.hashCode(), c4.hashCode());
+    assertNotEquals(c1.hashCode(), c3.hashCode());
+    assertNotEquals(c5.hashCode(), c4.hashCode());
   }
 }

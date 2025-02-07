ce49cb3a70b9e9c1178ec37f79236345be75c838
sl846 <shiyu.liu2@duke.edu>
2025-02-06 23:04:05 -0500

    added constructor with description String to Coordinate, passed tests; finished task3

--- app/src/main/java/edu/duke/sl846/battleship/Coordinate.java
+++ app/src/main/java/edu/duke/sl846/battleship/Coordinate.java
@@ -1,7 +1,7 @@
 package edu.duke.sl846.battleship;
 
 /**
- * Represents the coordinate of a given place.
+ * Represents the coordinate of a given place with row and column.
  */
 public class Coordinate {
   private final int row;
@@ -71,5 +71,36 @@ public class Coordinate {
   @Override
   public int hashCode() {
     return toString().hashCode();
+  }
+
+  /**
+   * Constructs a Coordinate from description String, e.g. A3
+   * 
+   * @param description is the description String. It should be composed of 2
+   *                    characters: the first is a-z or A-Z to indicate the row,
+   *                    and the second is 0-9 to indicate the column.
+   * @throws IllegalArgumentException if description String is either not of
+   *                                  length 2, not beginning with a-z / A-Z, or
+   *                                  not ending with 0-9.
+   */
+  public Coordinate(String description) {
+    if (description.length() != 2) {
+      throw new IllegalArgumentException("Coordinate description string should be of length 2, but is " + description);
+    }
+    String uppercaseDescription = description.toUpperCase();
+    char rowIndicator = uppercaseDescription.charAt(0);
+    if (rowIndicator < 'A' || rowIndicator > 'Z') {
+      throw new IllegalArgumentException(
+          "Coordinate description string should begin with a-z or A-Z, but is beginning with " + rowIndicator);
+    }
+    char colIndicator = uppercaseDescription.charAt(1);
+    if (colIndicator < '0' || colIndicator > '9') {
+      throw new IllegalArgumentException(
+          "Coordinate description string should end with 0-9, but is ending with " + colIndicator);
+    }
+    int rowNumber = rowIndicator - 'A';
+    int colNumber = Integer.parseInt(String.valueOf(colIndicator));
+    this.row = rowNumber;
+    this.column = colNumber;
   }
 }
--- app/src/test/java/edu/duke/sl846/battleship/CoordinateTest.java
+++ app/src/test/java/edu/duke/sl846/battleship/CoordinateTest.java
@@ -42,4 +42,32 @@ public class CoordinateTest {
     assertNotEquals(c1.hashCode(), c3.hashCode());
     assertNotEquals(c5.hashCode(), c4.hashCode());
   }
+
+  @Test
+  void test_string_constructor_valid_cases() {
+    Coordinate c1 = new Coordinate("B3");
+    assertEquals(1, c1.getRow());
+    assertEquals(3, c1.getColumn());
+    Coordinate c2 = new Coordinate("D5");
+    assertEquals(3, c2.getRow());
+    assertEquals(5, c2.getColumn());
+    Coordinate c3 = new Coordinate("A9");
+    assertEquals(0, c3.getRow());
+    assertEquals(9, c3.getColumn());
+    Coordinate c4 = new Coordinate("Z0");
+    assertEquals(25, c4.getRow());
+    assertEquals(0, c4.getColumn());
+  }
+
+  @Test
+  public void test_string_constructor_error_cases() {
+    assertThrows(IllegalArgumentException.class, () -> new Coordinate("00"));
+    assertThrows(IllegalArgumentException.class, () -> new Coordinate("AA"));
+    assertThrows(IllegalArgumentException.class, () -> new Coordinate("@0"));
+    assertThrows(IllegalArgumentException.class, () -> new Coordinate("[0"));
+    assertThrows(IllegalArgumentException.class, () -> new Coordinate("A/"));
+    assertThrows(IllegalArgumentException.class, () -> new Coordinate("A:"));
+    assertThrows(IllegalArgumentException.class, () -> new Coordinate("A"));
+    assertThrows(IllegalArgumentException.class, () -> new Coordinate("A12"));
+  }
 }

f85919aca48f9a8454299175eaeb56bf7dda3539
sl846 <shiyu.liu2@duke.edu>
2025-02-08 03:05:24 -0500

    passed test for occupiesCoordinates; finished task6

--- app/src/test/java/edu/duke/sl846/battleship/BasicShipTest.java
+++ app/src/test/java/edu/duke/sl846/battleship/BasicShipTest.java
@@ -6,8 +6,12 @@ import org.junit.jupiter.api.Test;
 
 public class BasicShipTest {
   @Test
-  public void test_() {
-
+  public void test_occupiesCoordinates() {
+    Coordinate c1 = new Coordinate(1, 3);
+    Coordinate c2 = new Coordinate(2, 4);
+    BasicShip s1 = new BasicShip(c1);
+    assertTrue(s1.occupiesCoordinates(c1));
+    assertFalse(s1.occupiesCoordinates(c2));
   }
 
 }

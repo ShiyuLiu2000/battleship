8fc9beebec7ba10dd32e32e99c9819a1e1c3ebbf
sl846 <shiyu.liu2@duke.edu>
2025-02-08 02:46:39 -0500

    created BasicShip class, auto-generated the methods required by Ship interface

---
+++ app/src/main/java/edu/duke/sl846/battleship/BasicShip.java
@@ -0,0 +1,35 @@
+package edu.duke.sl846.battleship;
+
+public class BasicShip implements Ship<Character> {
+
+  @Override
+  public boolean occupiesCoordinates(Coordinate where) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'occupiesCoordinates'");
+  }
+
+  @Override
+  public boolean isSunk() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'isSunk'");
+  }
+
+  @Override
+  public void recordHitAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'recordHitAt'");
+  }
+
+  @Override
+  public boolean wasHitAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'wasHitAt'");
+  }
+
+  @Override
+  public Character getDisplayInfoAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getDisplayInfoAt'");
+  }
+  
+}

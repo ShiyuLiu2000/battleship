d1c45bb96b3849b670c4cc17169aaf344dd9ee4f
sl846 <shiyu.liu2@duke.edu>
2025-02-09 00:27:04 -0500

    wrote tests for main in App, passed tests

--- app/src/test/java/edu/duke/sl846/battleship/AppTest.java
+++ app/src/test/java/edu/duke/sl846/battleship/AppTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.StringReader;
 
@@ -58,5 +59,33 @@ class AppTest {
     BoardTextView view = new BoardTextView(board);
     assertEquals(prompt + "\n" + view.displayMyOwnBoard(), bytes.toString());
     bytes.reset();
+  }
+
+  @Test
+  public void test_main() throws IOException {
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    PrintStream out = new PrintStream(bytes, true);
+    // get an InputStream for input.txt file
+    InputStream input = getClass().getClassLoader().getResourceAsStream("input.txt");
+    assertNotNull(input);
+    InputStream expectedStream = getClass().getClassLoader().getResourceAsStream("output.txt");
+    assertNotNull(expectedStream);
+    InputStream oldIn = System.in;
+    PrintStream oldOut = System.out;
+    try {
+      System.setIn(input);
+      System.setOut(out);
+      App.main(new String[0]);
+    }
+    finally {
+      System.setIn(oldIn);
+      System.setOut(oldOut);
+    }
+    // read all the data from expectedStream (output.txt)
+    String expected = new String(expectedStream.readAllBytes());
+    // get the String out of bytes
+    String actual = bytes.toString();
+    // finally, compare them
+    assertEquals(expected, actual);
   }
 }

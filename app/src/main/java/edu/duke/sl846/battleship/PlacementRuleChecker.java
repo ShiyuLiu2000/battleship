0b2f7cd249083ac03ac5061045e06a80f28fd3cb
sl846 <shiyu.liu2@duke.edu>
2025-02-10 22:22:07 -0500

    added documentation for PlacementRuleChecker

--- app/src/main/java/edu/duke/sl846/battleship/PlacementRuleChecker.java
+++ app/src/main/java/edu/duke/sl846/battleship/PlacementRuleChecker.java
@@ -1,15 +1,37 @@
 package edu.duke.sl846.battleship;
 
+/**
+ * Represents a placement rule checker for a {@link Board}.
+ */
 public abstract class PlacementRuleChecker<T> {
   private final PlacementRuleChecker<T> next;
 
+  /**
+   * Constructs a chained placement rule checker.
+   * 
+   * @param next is the next chained placement rule to be checked.
+   */
   public PlacementRuleChecker(PlacementRuleChecker<T> next) {
     this.next = next;
   }
 
-  // Subclasses will override this method to specify how they check their own rule
+  /**
+   * Abstracted method for future child PlacementRuleChecker classes to check
+   * their own rule.
+   * 
+   * @param theShip  is the Ship used to check the placement rule.
+   * @param theBoard is the Board used to check the placement rule.
+   * @return true if the placement is valid under this rule, false otherwise.
+   */
   protected abstract boolean checkMyRule(Ship<T> theShip, Board<T> theBoard);
 
+  /**
+   * Checks if the placement is valid using a chain of placement rules.
+   * 
+   * @param theShip  is the Ship used to check the placement rule.
+   * @param theBoard is the Board used to check the placement rule.
+   * @return true if the placement is valid under these rules, false otherwise.
+   */
   public boolean checkPlacement(Ship<T> theShip, Board<T> theBoard) {
     // if we fail our own rule: stop. The placement is not legal
     if (!checkMyRule(theShip, theBoard)) {

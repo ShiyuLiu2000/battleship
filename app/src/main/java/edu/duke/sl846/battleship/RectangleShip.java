c8e1196a3c1b1e4be2276a0604e225f1aed497f9
sl846 <shiyu.liu2@duke.edu>
2025-02-10 23:04:26 -0500

    updated documentation for RectangleShip class.

--- app/src/main/java/edu/duke/sl846/battleship/RectangleShip.java
+++ app/src/main/java/edu/duke/sl846/battleship/RectangleShip.java
@@ -3,11 +3,11 @@ package edu.duke.sl846.battleship;
 import java.util.HashSet;
 
 /**
- * Represents a rectangle Ship in our battleship game.
+ * Represents rectangle Ship in our battleship game.
  */
 public class RectangleShip<T> extends BasicShip<T> {
   private final String name;
-  
+
   /**
    * Makes a collection of Coordinates of the Ship.
    * 
@@ -29,27 +29,50 @@ public class RectangleShip<T> extends BasicShip<T> {
   }
 
   /**
-   * Constructs a RectangleShip with a Coordinate, width, and height.
+   * Constructs a RectangleShip with a Coordinate, width, height, and the
+   * view-specific display information of the Ship.
    * 
-   * @param upperLeft is the upper left Coordinate of the RectangleShip.
-   * @param width is the width of the RectangleShip.
-   * @param height is the height of the RectangleShip.
+   * @param upperLeft   is the upper left Coordinate of the RectangleShip.
+   * @param width       is the width of the RectangleShip.
+   * @param height      is the height of the RectangleShip.
+   * @param displayInfo is the view-specific information of the Ship.
    */
   public RectangleShip(String name, Coordinate upperLeft, int width, int height, ShipDisplayInfo<T> displayInfo) {
     super(makeCoordinates(upperLeft, width, height), displayInfo);
     this.name = name;
   }
 
-  // convinience constructor
+  /**
+   * Conviniently constructs a RectangleShip with a Coordinate, width, height, and
+   * the view-specific display information of the Ship.
+   * 
+   * @param upperLeft is the upper left Coordinate of the RectangleShip.
+   * @param width     is the width of the RectangleShip.
+   * @param height    is the height of the RectangleShip.
+   * @param data      is the view-specific information of the Ship when not hit.
+   * @param onHit     is the view-specific information of the Ship when hit.
+   */
   public RectangleShip(String name, Coordinate upperLeft, int width, int height, T data, T onHit) {
     this(name, upperLeft, width, height, new SimpleShipDisplayInfo<T>(data, onHit));
   }
 
-  public RectangleShip(Coordinate upperLeft, T data, T onHit)
-  {
+  /**
+   * Constructs a test RectangleShip with a Coordinate and the view-specific
+   * display information.
+   * 
+   * @param upperLeft is the upper left Coordinate of the RectangleShip.
+   * @param data      is the view-specific information of the Ship when not hit.
+   * @param onHit     is the view-specific information of the Ship when hit.
+   */
+  public RectangleShip(Coordinate upperLeft, T data, T onHit) {
     this("testship", upperLeft, 1, 1, data, onHit);
   }
 
+  /**
+   * Gets the name of the RectangleShip.
+   * 
+   * @return the name of the RectangleShip.
+   */
   @Override
   public String getName() {
     return name;

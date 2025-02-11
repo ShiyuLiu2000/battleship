3628091b3c259152421dddd7ebecb369f9ed3024
sl846 <shiyu.liu2@duke.edu>
2025-02-10 22:54:54 -0500

    updated documentation for BasicShip.class

--- app/src/main/java/edu/duke/sl846/battleship/BasicShip.java
+++ app/src/main/java/edu/duke/sl846/battleship/BasicShip.java
@@ -2,15 +2,20 @@ package edu.duke.sl846.battleship;
 
 import java.util.HashMap;
 
+/**
+ * A basic kind of Ship.
+ */
 public abstract class BasicShip<T> implements Ship<T> {
   // put all coordinates the ship occupies, and track which ones have been hit
   protected HashMap<Coordinate, Boolean> myPieces;
   protected ShipDisplayInfo<T> myDisplayInfo;
 
   /**
-   * Constructs a BasicShip with given locations.
+   * Constructs a BasicShip with given locations and given display info settings.
    * 
-   * @param where is the Iterable of the Coordinates that make up the BasicShip.
+   * @param where         is the Iterable of the Coordinates that make up the
+   *                      BasicShip.
+   * @param myDisplayInfo is the information display settings for the Ship.
    */
   public BasicShip(Iterable<Coordinate> where, ShipDisplayInfo<T> myDisplayInfo) {
     this.myPieces = new HashMap<Coordinate, Boolean>();
@@ -31,9 +36,15 @@ public abstract class BasicShip<T> implements Ship<T> {
     return myPieces.containsKey(where);
   }
 
+  /**
+   * Check if this ship has been hit in all of its locations meaning it has been
+   * sunk.
+   * 
+   * @return true if this ship has been sunk, false otherwise.
+   */
   @Override
   public boolean isSunk() {
-    for (Coordinate c: myPieces.keySet()) {
+    for (Coordinate c : myPieces.keySet()) {
       if (wasHitAt(c) == false) {
         return false;
       }
@@ -41,35 +52,70 @@ public abstract class BasicShip<T> implements Ship<T> {
     return true;
   }
 
+  /**
+   * Make this ship record that it has been hit at the given coordinate. The
+   * specified coordinate must be part of the ship.
+   * 
+   * @param where specifies the coordinates that were hit.
+   * @throws IllegalArgumentException if where is not part of the Ship
+   */
   @Override
   public void recordHitAt(Coordinate where) {
     checkCoordinateInThisShip(where);
     myPieces.put(where, true);
   }
 
+  /**
+   * Check if this ship was hit at the specified coordinates. The coordinates must
+   * be part of this Ship.
+   * 
+   * @param where is the coordinates to check.
+   * @return true if this ship as hit at the indicated coordinates, and false
+   *         otherwise.
+   * @throws IllegalArgumentException if the coordinates are not part of this
+   *                                  ship.
+   */
   @Override
   public boolean wasHitAt(Coordinate where) {
     checkCoordinateInThisShip(where);
     if (myPieces.get(where) == true) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   }
 
+  /**
+   * Return the view-specific information at the given coordinate. This coordinate
+   * must be part of the ship.
+   * 
+   * @param where is the coordinate to return information for
+   * @throws IllegalArgumentException if where is not part of the Ship
+   * @return The view-specific information at that coordinate.
+   */
   @Override
   public T getDisplayInfoAt(Coordinate where) {
     boolean wasHit = wasHitAt(where);
     return myDisplayInfo.getInfo(where, wasHit);
   }
 
+  /**
+   * Helps to check if a given Coordinate is inside the BasicShip.
+   * 
+   * @param c is the Coordinate to be checked.
+   * @throws IllegalArgumentException if the Coordinate is not in the ship.
+   */
   public void checkCoordinateInThisShip(Coordinate c) {
     if (myPieces.containsKey(c) == false) {
       throw new IllegalArgumentException("The Coordinate " + c.toString() + " is not in the ship.");
     }
   }
 
+  /**
+   * Get all of the Coordinates that this Ship occupies.
+   * 
+   * @return an Iterable with the Coordinates that this Ship occupies.
+   */
   @Override
   public Iterable<Coordinate> getCoordinates() {
     return myPieces.keySet();

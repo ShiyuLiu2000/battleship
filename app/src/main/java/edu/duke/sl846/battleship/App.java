18b8a9c71153780b428c11d389c95812de2be292
sl846 <shiyu.liu2@duke.edu>
2025-02-18 22:48:42 -0500

    make App reads in player type at the beginning of the game, passed tests by manually choosing to have 2 computer players

--- app/src/main/java/edu/duke/sl846/battleship/App.java
+++ app/src/main/java/edu/duke/sl846/battleship/App.java
@@ -5,6 +5,7 @@ package edu.duke.sl846.battleship;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
@@ -37,11 +38,46 @@ public class App {
     player2.doPlacementPhase();
   }
 
+  /**
+   * Runs the attacking phase of the battleship game.
+   */
   public void doAttackingPhase() throws IOException {
     boolean isGameEnd = false;
     while (!isGameEnd) {
       isGameEnd = player1.playOneTurnAgainst(player2) || player2.playOneTurnAgainst(player1);
     }
+  }
+
+  static TextPlayer readTextPlayer(String name, Board<Character> theBoard, BufferedReader inputReader, PrintStream out)
+      throws IOException {
+    TextPlayer ans = null;
+    V2ShipFactory factory = new V2ShipFactory();
+    while (ans == null) {
+      out.print("Please choose the type (H for human, C for computer) for Player " + name + ": ");
+      String s = inputReader.readLine();
+      if (s == null) {
+        throw new EOFException("The input for player type is empty.");
+      }
+      s = s.toUpperCase();
+      if (s.length() != 1) {
+        out.print("Expect the input for player type to be length 1, but is " + s.length() + ".\n");
+        continue;
+      }
+      char playerType = s.charAt(0);
+      if (playerType != 'H' && playerType != 'C') {
+        out.print("Player type can only be human (h/H) or computer (c/C), but is " + playerType + ".\n");
+        continue;
+      }
+      switch (playerType) {
+        case 'H':
+          ans = new HumanTextPlayer(name, theBoard, inputReader, out, factory);
+          break;
+        case 'C':
+          ans = new ComputerTextPlayer(name, theBoard, inputReader, out, factory);
+          break;
+      }
+    }
+    return ans;
   }
 
   /**
@@ -51,9 +87,8 @@ public class App {
     Board<Character> board1 = new BattleShipBoard<>(10, 20, 'X');
     Board<Character> board2 = new BattleShipBoard<>(10, 20, 'X');
     BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-    V2ShipFactory factory = new V2ShipFactory();
-    HumanTextPlayer player1 = new HumanTextPlayer("A", board1, input, System.out, factory);
-    ComputerTextPlayer player2 = new ComputerTextPlayer("B", board2, input, System.out, factory);
+    TextPlayer player1 = App.readTextPlayer("A", board1, input, System.out);
+    TextPlayer player2 = App.readTextPlayer("B", board2, input, System.out);
     App app = new App(player1, player2);
 
     app.doPlacementPhase();
